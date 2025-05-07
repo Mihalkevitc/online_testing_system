f7c3345c676b722bcd40a7c701be21c8ac0c8f75
Mihalkevitc <144014512+Mihalkevitc@users.noreply.github.com>
2025-05-07 12:55:25 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,8 +92,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # }
 
+# Настройки базы данных
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
