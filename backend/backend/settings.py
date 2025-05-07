8a63dea5c14ed118799dd2ec10f06bd2b3b8cb1c
Mihalkevitc <144014512+Mihalkevitc@users.noreply.github.com>
2025-05-07 12:41:16 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import sys
+print("DATABASE_URL:", os.getenv("DATABASE_URL"), file=sys.stderr)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
